1c2488973bcc08a28214bbe7dc29a2e67650698f
gathoni-k <maryg.kahiga@gmail.com>
2022-08-04 07:10:27 +0300

    Create chrome extension

---
+++ README.md
@@ -0,0 +1,10 @@
+# Save URL
+A chrome extension that uses the Chrome API to save the current url in local storage.
+
+![Tux, the Linux mascot](/public/demo.png)
+
+## How to Use
+Clone this repo and deploy it to chrome through 
+[Chrome extensions developer dashboard](chrome://extensions/)
+
+Click the save button to save url and double click delete button to delete all URLs
---
+++ index.js
@@ -0,0 +1,39 @@
+let myLeads = []
+const ulEl = document.getElementById("ul-el")
+
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push({url: tabs[0].url, title: tabs[0].title})
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i].url}'>
+                    ${leads[i].title}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
